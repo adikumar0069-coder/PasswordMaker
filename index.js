53e761646c3b612283f69b2308b6ddfd86f9475b
adi <adikumar0069@gmail.com>
2026-04-16 18:31:35 +0530

    adding necessary data

---
+++ index.js
@@ -0,0 +1,12 @@
+/* composition of password loweralph = 25%(26), upperalph = 25%(26), specialSymb = 25%(28), numbers = 25%(10);
+step 1 - select the given percentage of element randomly form given arry
+step 2 - concatinate the selected element 
+step 3 - suffle them rendomly */
+const specialSymb = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ';', ':', '"', ',', '.', '<', '>', '/', '?'];
+const upperalph = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+const loweralph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const numbers =  ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
